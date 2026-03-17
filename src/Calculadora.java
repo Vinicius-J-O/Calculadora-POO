da29225f628cafb5f3a28f9880394c326e368ca5
Vinicius-J-O <viniciusjesusdeoliveirapro@gmail.com>
2026-03-17 11:00:38 -0300

    Add files via upload

---
+++ src/Calculadora.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Calculadora {
+    private JFrame janela;
+    private JTextField visorAtual;
+    private JTextField visorEquacao;
+    public Calculadora (){
+        janela = new JFrame();
+        
+        janela.setSize(400, 500);
+        janela.setTitle("Calculadora");
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JPanel painelVisores = new JPanel(new GridLayout(2, 1));
+        visorEquacao = new JTextField();
+        visorEquacao.setEditable(false);
+        visorEquacao.setHorizontalAlignment(JTextField.RIGHT);
+        visorEquacao.setBackground(Color.ORANGE);
+
+        visorAtual = new JTextField("0");
+        visorAtual.setEditable(false);
+        visorAtual.setHorizontalAlignment(JTextField.RIGHT);
+        visorAtual.setFont(new Font("Arial", Font.BOLD, 28));
+
+        painelVisores.add(visorEquacao);
+        painelVisores.add(visorAtual);
+
+        JPanel painelBotoes = new JPanel(new GridLayout(6, 4));
+        String[] textos = {
+            "%", "CE", "C", "{}",
+            "1/x", "x2", "2{x", "/",
+            "7", "8", "9", "x",
+            "4", "5", "6", "-",
+            "1", "2", "3", "+",
+            "+/-", "0", ",", "=",
+        };
+
+        for (String texto : textos) {
+            JButton btn = new JButton(texto);
+            btn.addActionListener(null);
+            painelBotoes.add(btn);
+        }
+
+        
+
+        janela.add(painelBotoes);
+        janela.add(painelVisores, BorderLayout.NORTH);
+        janela.setVisible(true);
+    }
+}
